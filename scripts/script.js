cc67b45d91126bc70beda2881aa030fc83350fdd
TGorgijoska <gorgijoska.t@hotmail.com>
2021-09-19 00:47:34 +0200

    changed functions, added eraser button

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,21 @@
 const container = document.querySelector('.container');
 const clearBtn = document.querySelector('.btn-clear');
-const buttons = document.querySelectorAll('.btn');
+const randomBtn = document.querySelector('.btn-random');
+const eraserBtn = document.querySelector('.btn-eraser');
 
 const containerStyles = window.getComputedStyle(container);
 let conatinerDimensitions = (containerStyles.width).slice(0,-2);
-let squareColor, color, dimension, squareSum;
+let squareColor, color, dimension, squaresSum, currentColor = "color";
 
+clearBtn.onclick = () => askUser();
+randomBtn.onclick = () => updateColor('random');
+eraserBtn.onclick = () => updateColor('eraser');
 createGrid(16);
+
 function calcSqaresDimension(squares){
     return (conatinerDimensitions / squares) + 'px';
 }
-// ========= GRID CREATION
+
 function createGrid (squares){
     container.innerHTML = "";  
     dimension = calcSqaresDimension(squares);
@@ -20,33 +25,35 @@ function createGrid (squares){
         let gridEl = document.createElement('div');
         gridEl.setAttribute('class', 'grid');      
         gridEl.setAttribute('style', `width:${dimension}; height:${dimension}`);
+        gridEl.addEventListener('mouseover', changeColor);
         container.appendChild(gridEl);
     }
-    gridColorEvent();
 }
+
 function random_rgba() {
     let o = Math.round, r = Math.random, s = 255;
     return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
 }
-function gridColorEvent (shades){
-    const grid = document.querySelectorAll('.grid');
-    grid.forEach(gridEl => gridEl.addEventListener('mouseover', () => {
-        if(shades == 'random'){
-            color = random_rgba();
-            gridEl.setAttribute('style', `width:${dimension}; height:${dimension};background-color: ${color}`);
-        }
-        else if(shades == 'shades'){
-            
-        } 
-        else {
-            gridEl.classList.add('color');
-        }
-    }))
+
+function changeColor(e){
+    if(currentColor == 'color'){
+        e.target.style.backgroundColor = "#fdffc4";
+    } else if (currentColor == 'random'){
+        color = random_rgba();
+        e.target.style.backgroundColor = color;
+    } else if (currentColor == 'shades'){
+        // e.target.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`
+    } else if(currentColor == 'eraser'){
+        e.target.style.backgroundColor = "#abd1ff";
+    }
+
 }
 
-// ========= EVENT LISTENERS
+function updateColor(color){
+    currentColor = color;
+}
 
-clearBtn.addEventListener('click', () => {   
+function askUser(){ 
     let squares = prompt("Enter a number of squares (max 100)", '16');
     if(squares > 100){
         squares = prompt("You can make max of 100x100. Enter a new number", '16');
@@ -55,16 +62,4 @@ clearBtn.addEventListener('click', () => {
         createGrid(squares);
     }
     
-})
-
-buttons.forEach(btn => btn.addEventListener('click', ()=>{
-    switch (btn.dataset.color){
-        case 'random': 
-            gridColorEvent('random');
-            break;
-        case 'shades':
-            gridColorEvent('shades');
-            break;
-    }
-   
-}))
+}
